8d4786d01c3535bdbd990c4cc4f4af302c42cc15
ms63h38 <146327933+ms63h38@users.noreply.github.com>
2026-09-10 18:36:01 +0200

    Bump Trail Coach 2.7 cache after decoder fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="trailcoach-2-v2701";
+const CACHE="trailcoach-2-v2702";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
